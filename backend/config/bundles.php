efaf6260557918bf706db14d1b2c686e10b9302e
zenbout-mohamed <mohamed.zenbout@laplateforme.io>
2026-05-20 10:57:40 +0200

    feat: add Swagger OpenAPI documentation

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -9,4 +9,7 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
